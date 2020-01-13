5d153bf9563dc6ef0b69daf95fd75350b15804eb
TonyCha3713 <tonyjcha3713@gmail.com>
2020-01-12 22:11:33 -0800

    first PID

--- Dec-14-PV-Comp-Code/First_Comp/src/Autons/redSmallZone8.cpp
+++ Dec-14-PV-Comp-Code/First_Comp/src/Autons/redSmallZone8.cpp
@@ -2,31 +2,4 @@
 void redSmallZone8()
 {
     moveBasePID(1000,1000,6000);
-    //moveBase(1000,6000);
-    /*runIntake(200);
-    moveBase(1255,75);
-    delay(3000);
-    turnBase(-25,75);
-    delay(300);
-    moveBase(-1300,170);
-    delay(2150);
-    turnBase(55,75);
-    delay(300);
-    moveBase(1255,75);
-    delay(3000);
-    moveBase(-900,170);
-    runTray(30);
-    delay(1600);
-    turnBase(400,150);
-    delay(600);
-    moveBase(500,100);
-    delay(600);
-    runIntake(0);
-    runTray(150);
-    delay(2000);
-    runIntake(-100);
-    delay(500);
-    moveBase(-400,150);
-*/
-    
 }
